3360216f7ea121330d70a31abcf32a98420c14f7
U Prince <uprince@falabella.cl>
2019-05-29 12:36:23 +0530

    feature branch first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # git-demo
 This repo is for practicing the git commands like branching, rebase, squash etc
+
+This is feature1 branch
--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 const sum = (a,b) => a+b;
 
 const diff = (a,b) => b-a;
+
+const multiply = (a,b) => a*b;
+
+const div = (a,b) => b/a;
